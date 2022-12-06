5feca294fac42654fe6f6fec267588ca9b9851f0
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2022-12-06 19:03:15 +0100

    Update img_processing.py

--- img_processing.py
+++ img_processing.py
@@ -2,6 +2,21 @@ import cv2
 import numpy as np
 
 
+# from rembg import remove
+
+# input_path = './osef.jpeg'
+# output_path = './test-output.jpeg'
+
+
+# print("processing ...")
+# with open(input_path, 'rb') as i:
+#     with open(output_path, 'wb') as o:
+#         print("OK")
+#         t = i.read()
+#         print(t)
+#         output = remove(t)
+#         o.write(output)
+
 # def remove_background(url):
 #     # load image
 #     img = cv2.imread(url)
