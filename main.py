72851780a13541248c332818f5abf0f0fe868596
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-01-02 18:55:33 +0100

    [FIX]

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def app():
 @app.command(name="create-product")
 @click.option("--name", prompt="Name", required=True)
 @click.option("--description", prompt="Description", required=True)
-@click.option("--image_url", prompt="Url image", required=True)
+@click.option("--image_url", prompt="Url image (not uploaded to stripe !)", required=True)
 @click.option("--shippable", prompt="Shippable", default=True, required=True)
 @click.option("--statement_descriptor", prompt="Name on bank statement (5chars)", required=True, )
 @click.option("--remove_image_background", prompt="Remove image background",default=True, required=True)
@@ -76,7 +76,7 @@ def create_product_stripe(name, description, image_url, shippable, statement_des
         product = stripe.Product.create(
             name=name,
             description=description,
-            images=[f"/dist/{PIC_NO_BG_NAME}"], # Is not uploading for some reason, do it manually after generation on stripe dashboard ...
+            #images=[f"/dist/{PIC_NO_BG_NAME}"], # Is not uploading for some reason, do it manually after generation on stripe dashboard ...
             shippable=shippable,
             statement_descriptor=statement_descriptor,
             tax_code=tax_code,
